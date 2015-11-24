7d2c6ea245f41241e64980b4b014f6a0623348da
Chad Tolkien <chad@sodadigital.com.au>
2015-11-24 18:48:01 +1100

    swap to more basic idisposable pattern

--- src/TinyPNG/TinyPng.cs
+++ src/TinyPNG/TinyPng.cs
@@ -115,39 +115,18 @@ namespace TinyPngApi
 
 
         #region IDisposable Support
-        private bool disposedValue = false; // To detect redundant calls
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
 
         protected virtual void Dispose(bool disposing)
         {
-            if (!disposedValue)
+            if (disposing)
             {
-                if (disposing)
-                {
-                    // TODO: dispose managed state (managed objects).
-                    httpClient.Dispose();
-                }
-
-                // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
-                // TODO: set large fields to null.
-
-                disposedValue = true;
+                httpClient?.Dispose();
             }
-        }
-
-        // TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
-        ~TinyPng()
-        {
-            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
-            Dispose(false);
-        }
-
-        // This code added to correctly implement the disposable pattern.
-        public void Dispose()
-        {
-            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
-            Dispose(true);
-            // TODO: uncomment the following line if the finalizer is overridden above.
-            GC.SuppressFinalize(this);
         }
         #endregion
     }
