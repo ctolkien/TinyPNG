b6d3ba92c4bfcf4834490f32cb363ecb83f24b75
Chad Tolkien <chad@sodadigital.com.au>
2016-01-28 17:38:45 +1100

    yaml kill me now

--- aspnetcore-version.js
+++ aspnetcore-version.js
@@ -2,18 +2,19 @@ var jsonfile = require('jsonfile');
 var semver = require('semver');
 
 var file = './src/tinypng/project.json';
-var buildVersion = process.env.APPVEYOR_BUILD_VERSION.substring(1);
-
-var findPoint       = buildVersion.lastIndexOf(".");
-var basePackageVer  = buildVersion.substring(0, findPoint);
-var buildNumber     = buildVersion.substring(findPoint + 1, buildVersion.length);
-var semversion      = semver.valid(basePackageVer + "." + buildNumber)
 
 jsonfile.readFile(file, function (err, project) {
-    console.error(err);
-    project.version = semversion;
+    if (err)
+    {
+        console.error("Couldn't open project.json");
+        return;
+    }
+    console.log(process.env.toString());
+    project.version = process.env.APPVEYOR_BUILD_VERSION;
     jsonfile.writeFile(file, project, {spaces: 2}, function(err) {
-        console.error("We've failed to update the build number");
-        console.error(err);
+        if (err)
+        {
+            console.error("We've failed to update the build number");
+        }
     });
 })
