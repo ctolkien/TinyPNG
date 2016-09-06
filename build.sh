cfd7660c5c4b37084569ca6545d0d009c3316fed
Chad Tolkien <chad@tolkien.id.au>
2016-09-06 12:32:14 +1000

    line endings, etc

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
-
 #!/usr/bin/env bash
-Folder="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+repoFolder="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $repoFolder
 
 koreBuildZip="https://github.com/aspnet/KoreBuild/archive/dev.zip"
@@ -45,31 +44,3 @@ if test ! -d $buildFolder; then
 fi
 
 $buildFile -r $repoFolder "$@"
-
-#exit if any command fails
-set -e
-
-artifactsFolder="./artifacts"
-
-if [ -d $artifactsFolder ]; then  
-  rm -R $artifactsFolder
-fi
-
-dotnet restore
-
-# Ideally we would use the 'dotnet test' command to test netcoreapp and net451 so restrict for now 
-# but this currently doesn't work due to https://github.com/dotnet/cli/issues/3073 so restrict to netcoreapp
-
-dotnet test ./tests/TinyPNG.Tests -c Release -f netcoreapp1.0
-
-# Instead, run directly with mono for the full .net version 
-dotnet build ./tests/TinyPNG.Tests -c Release -f net451
-
-mono \  
-./tests/TinyPNG.Tests/bin/Release/net451/*/dotnet-test-xunit.exe \
-./tests/TinyPNG.Tests/bin/Release/net451/*/TEST_PROJECT_NAME.dll
-
-revision=${TRAVIS_JOB_ID:=1}  
-revision=$(printf "%04d" $revision) 
-
-dotnet pack ./src/TinyPNG -c Release -o ./artifacts --version-suffix=$revision  
