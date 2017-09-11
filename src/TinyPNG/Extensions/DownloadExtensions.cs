b9555872ee2eff350244824afebab75c6047c5c5
Mitchell Kutchuk <mitch528@gmail.com>
2017-09-10 20:46:50 -0700

    Add support for preserve metadata (#22)

--- src/TinyPNG/Extensions/DownloadExtensions.cs
+++ src/TinyPNG/Extensions/DownloadExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
 using TinyPng.Responses;
@@ -9,19 +10,22 @@ namespace TinyPng
 
     public static class DownloadExtensions
     {
+        private const string JpegType = "image/jpeg";
+
         /// <summary>
         /// Downloads the result of a TinyPng Compression operation
         /// </summary>
         /// <param name="compressResponse"></param>
+        /// <param name="metadata"></param>
         /// <returns></returns>
-        public async static Task<TinyPngImageResponse> Download(this Task<TinyPngCompressResponse> compressResponse)
+        public async static Task<TinyPngImageResponse> Download(this Task<TinyPngCompressResponse> compressResponse, PreserveMetadata metadata = PreserveMetadata.None)
         {
             if (compressResponse == null)
                 throw new ArgumentNullException(nameof(compressResponse));
 
             var compressResult = await compressResponse;
 
-            return await Download(compressResult);
+            return await Download(compressResult, metadata);
 
         }
 
@@ -29,15 +33,19 @@ namespace TinyPng
         /// Downloads the result of a TinyPng Compression operation
         /// </summary>
         /// <param name="compressResponse"></param>
+        /// <param name="metadata"></param>
         /// <returns></returns>
-        public async static Task<TinyPngImageResponse> Download(TinyPngCompressResponse compressResponse)
+        public async static Task<TinyPngImageResponse> Download(TinyPngCompressResponse compressResponse, PreserveMetadata metadata = PreserveMetadata.None)
         {
             if (compressResponse == null)
                 throw new ArgumentNullException(nameof(compressResponse));
 
-            var msg = new HttpRequestMessage(HttpMethod.Get, compressResponse.Output.Url);
+            var msg = new HttpRequestMessage(HttpMethod.Get, compressResponse.Output.Url)
+            {
+                Content = CreateContent(metadata, compressResponse.Output.Type)
+            };
 
-            var response = await compressResponse.HttpClient.SendAsync(msg);
+            var response = await compressResponse.HttpClient.SendAsync(msg).ConfigureAwait(false);
 
             if (response.IsSuccessStatusCode)
             {
@@ -46,6 +54,37 @@ namespace TinyPng
 
             var errorMsg = JsonConvert.DeserializeObject<ApiErrorResponse>(await response.Content.ReadAsStringAsync());
             throw new TinyPngApiException((int)response.StatusCode, response.ReasonPhrase, errorMsg.Error, errorMsg.Message);
+        }
+
+        private static HttpContent CreateContent(PreserveMetadata metadata, string type)
+        {
+            if (metadata == PreserveMetadata.None)
+                return null;
+
+            var preserve = new List<string>();
+
+            if (metadata.HasFlag(PreserveMetadata.Copyright))
+            {
+                preserve.Add("copyright");
+            }
+            if (metadata.HasFlag(PreserveMetadata.Creation))
+            {
+                if (type != JpegType)
+                    throw new InvalidOperationException($"Creation metadata can only be preserved with type {JpegType}");
+
+                preserve.Add("creation");
+            }
+            if (metadata.HasFlag(PreserveMetadata.Location))
+            {
+                if (type != JpegType)
+                    throw new InvalidOperationException($"Location metadata can only be preserved with type {JpegType}");
+
+                preserve.Add("location");
+            }
+
+            string json = JsonConvert.SerializeObject(new { preserve });
+
+            return new StringContent(json, System.Text.Encoding.UTF8, "application/json");
         }
     }
 }
